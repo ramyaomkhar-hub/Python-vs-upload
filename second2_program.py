c4784197c62989631aa03fe4d268710f4ea968a3
ramyaomkhar-hub <ramyaomkhar@gmail.com>
2026-08-24 15:37:59 +0530

    https://github.com/ramyaomkhar-hub/Python-vs-upload.git

---
+++ second2_program.py
@@ -0,0 +1,12 @@
+#variables
+#print("a")#using string by ""it gives what is a .>> output:a
+#print(a)#using a as a variable it gives what is inside the a . >> output:10
+
+# or
+a = "ramya"
+b = "majunth"
+
+age = 20
+age = 40
+
+print("ramya")
