6d409ba919ef395b2c132fefa53cd20bfc3cd778
ramyaomkhar-hub <ramyaomkhar@gmail.com>
2026-08-24 21:33:06 +0530

    update python code

--- Data_type_in _python.py
+++ Data_type_in _python.py
@@ -1,9 +1,15 @@
 #Data type in python
+
+# int:for integers(e.g,.1,-3,)
+#float:for floating-point numbers(e.g,.3.14)
+#str:for string (e.g,."Hello","python")
+#bool:for boolean values(True or false)
 name = "ramya" #string
 age = 20 #int
 is_student = True #bool
 weight = 56 #float
 
+#type checking 
 print(type(weight))
 print(type(is_student))
 
