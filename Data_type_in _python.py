ac70499f25495e81b2363604536e157c52654bcc
ramyaomkhar-hub <ramyaomkhar@gmail.com>
2026-08-24 20:44:40 +0530

    https://github.com/ramyaomkhar-hub/Python-vs-upload.git

---
+++ Data_type_in _python.py
@@ -0,0 +1,12 @@
+#Data type in python
+name = "ramya" #string
+age = 20 #int
+is_student = True #bool
+weight = 56 #float
+
+print(type(weight))
+print(type(is_student))
+
+age_float = float(age)
+print(age_float)
+
