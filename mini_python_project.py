97d4229e2ef12fb4b4638e7af6c39d152d3a254c
ramyaomkhar-hub <ramyaomkhar@gmail.com>
2026-08-25 12:42:32 +0530

    upload python code

--- mini_python_project.py
+++ mini_python_project.py
@@ -14,4 +14,11 @@ age_diff = abs(boy_age - girl_name)
 
 print(f"{boy_name} loves {girl_name} Age difference is{age_diff} years")
 
+#output
+#Boy name : Ram
+#Boy age : 25
+#Girl name : sita
+#Girl age : 24
+# Ram loves sita
+
 
