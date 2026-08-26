1100cddb3a1e1717cf9114fee3c577c2f0910a3a
ramyaomkhar-hub <ramyaomkhar@gmail.com>
2026-08-26 21:26:20 +0530

    https://github.com/ramyaomkhar-hub/Python-vs-upload.git

---
+++ Lists_in_python.py
@@ -0,0 +1,16 @@
+#Lists
+
+items = ["apple","banana","cherry","apple1"]
+
+print(items)
+print(items[0])    #indexing 
+
+#change
+items.pop()  #remove one iteam
+print(items)
+items.append("orange") #add iteams
+items.remove("apple")
+
+items.insert ( "plate")
+print("items")
+
